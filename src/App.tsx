db8b2ae0bab5995fc11ddf235bc8c1674c268048
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-02-01 00:04:39 +0400

    change colors

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,14 @@ const cardsData: TCard[] = [
 
 function Card({ imgSrc, title, description, idx }: TCard) {
   return (
-    <div className="flex flex-row gap-5 h-32 md:h-44 mt-8">
+    <div className="flex flex-row gap-5 h-44 mt-8 order-5">
       <img src={imgSrc} />
       <div className="flex flex-col justify-between">
-        <p className="text-gray-300 text-4xl md:text-4xl font-semibold">0{idx}</p>
-        <p className="font-bold text-2xl md:text-xl">{title}</p>
-        <p className="text-gray-500 text-md md:text-black">{description}</p>
+        <p className="text-gray-300 text-4xl md:text-4xl font-semibold">
+          0{idx}
+        </p>
+        <p className="font-bold text-xl">{title}</p>
+        <p className="text-gray-500 text-md md:text-blueGray">{description}</p>
       </div>
     </div>
   );
@@ -39,16 +41,18 @@ function Card({ imgSrc, title, description, idx }: TCard) {
 
 function App() {
   return (
-    <div className="mx-8 md:m-20 mb-20">
+    <div className="mx-8 md:mx-44 mb-20">
       <NavBar />
-      <main className="w-full h-full gap-10 mb-28 grid grid-cols-1 lg:grid-cols-3">
-        <div className="lg:col-span-2">
+      <main className="w-full h-full gap-12 mb-28 grid grid-cols-1 lg:grid-cols-3">
+        <div className="lg:col-span-2 order-1">
           <img src="./images/image-web-3-desktop.jpg" alt="" />
         </div>
-        <div className="flex flex-col p-10 justify-between lg:row-span-2 bg-blueGray gap-10 text-white">
-          <h2 className="text-5xl font-semibold text-yellow-500">New</h2>
+        <div className="flex flex-col p-10 order-4 lg:order-2 justify-between lg:row-span-2 bg-blueGray gap-10 text-[#FEFDF9]">
+          <h2 className="text-3xl lg:text-4xl font-semibold text-yellow-500">
+            New
+          </h2>
           <div>
-            <h3 className="text-xl font-bold pb-2">
+            <h3 className="text-xl text-[#FEFDF9] text font-bold pb-2">
               Hydrogen VS Electric Cars
             </h3>
             <p className="text-sm text-gray-400">
@@ -57,7 +61,7 @@ function App() {
           </div>
           <div className="bg-gray-100 h-[0.5px]"></div>
           <div>
-            <h3 className="text-xl font-bold pb-2">
+            <h3 className="text-xl text-[#FEFDF9] font-bold pb-2">
               The Downsides of AI Artistry
             </h3>
             <p className="text-sm text-gray-400">
@@ -67,23 +71,25 @@ function App() {
           </div>
           <div className="bg-gray-100 h-[0.5px]"></div>
           <div>
-            <h3 className="text-xl font-bold pb-2">is VC Funding Drying Up?</h3>
+            <h3 className="text-xl text-[#FEFDF9] font-bold pb-2">
+              is VC Funding Drying Up?
+            </h3>
             <p className="text-sm text-gray-400">
               Private funding by VC firms is down 50% YOY. We take a look at
               what that means.
             </p>
           </div>
         </div>
-        <h1 className="font-extrabold md:text-4xl text-6xl pr-8">
+        <h1 className="font-extrabold text-blueGray order-2 lg:order-3 text-4xl md:text-6xl lg:text-7xl">
           The Bright Future of Web 3.0?
         </h1>
-        <div className="flex flex-col gap-10 justify-between">
-          <p className="text-lg text-gray-500">
+        <div className="flex flex-col gap-10 order-3 lg:order-4 justify-between">
+          <p className="text-xl text-gray-500">
             We dive into the next evolution of the web that claims to but the
             power of the plattorms back into the hands of the people. But is it
             really fulfilling its promise?
           </p>
-          <button className="hover:bg-red-400 hover tracking-widest bg-red-500 px-14 md:px-12 text-xl md:text-lg py-4 md:py-3 w-fit text-white">
+          <button className="hover:bg-red-400 hover tracking-widest bg-red-500 px-14 md:px-12 text-xl md:text-lg py-4 md:py-3 w-fit text-[#FEFDF9]">
             READ MORE
           </button>
         </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -29,19 +29,21 @@ const NavBar = () => {
       </div>
       <div onClick={toggleMenu} className="lg:hidden cursor-pointer block">
         {!menu ? (
-          <div className="w-10">
-            <img src="./images/icon-menu.svg" alt="menu" />
-          </div>
+          <svg width="40" height="17" xmlns="http://www.w3.org/2000/svg">
+            <g fill="#00001A" fill-rule="evenodd">
+              <path d="M0 0h40v3H0zM0 7h40v3H0zM0 14h40v3H0z" />
+              <path d="M0 0h40v3H0z" />
+            </g>
+          </svg>
         ) : (
           <>
-            <div className="w-10 h-10">
-              <img
-                className="pb-4"
-                src="./images/icon-menu-close.svg"
-                alt="menu"
-              />
-            </div>
-            <div className="bg-white border-2 border-blueGray gap-2 text-blueGray px-10 py-6 text-lg flex flex-col absolute -translate-x-36">
+            <svg width="32" height="31" xmlns="http://www.w3.org/2000/svg">
+              <g fill="#00001A" fill-rule="evenodd">
+                <path d="m2.919.297 28.284 28.284-2.122 2.122L.797 2.419z" />
+                <path d="M.797 28.581 29.081.297l2.122 2.122L2.919 30.703z" />
+              </g>
+            </svg>
+            <div className="bg-white border-2 border-blueGray gap-2 text-blueGray px-10 py-6 text-lg flex flex-col absolute -translate-x-[8.6rem] translate-y-4">
               <a className="hover:text-blueGray" href="">
                 Home
               </a>
